f7888c72176a175cbbe4838603ec586719cc2f4c
Linnix11 <arthur.champagne-forterre@epitech.digital>
2024-09-30 13:19:28 +0200

    ex_6.js

---
+++ ex_06.js
@@ -0,0 +1,9 @@
+if (bananasCount === 0) {
+    console.log("Oh no, there is no banana.");
+} 
+	else if (bananasCount > 0) {
+    console.log("Yummy!");
+} 
+	else {
+    console.log("Do I owe you bananas?");
+    }
